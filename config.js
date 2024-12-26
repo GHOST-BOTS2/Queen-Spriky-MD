43cc0a5780017495c7daccb412d0eceb4c2e58ea
GHOST-MD <stataskindcyberghost@gmail.com>
2024-12-26 19:48:36 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || '', // Enter Your Session ID
-    MONGODB: process.env.MONGODB || '',// Enter Your MongoDB URL
-    OWNER_NUMBER: process.env.OWNER_NUMBER || '',    // Enter Your Owner Number
+    SESSION_ID: process.env.SESSION_ID || 'uOpEwAza#59JIX_00JWIiTfyXEhBeI1jcDKywHOzaVgWUI3IeQ6E', // Enter Your Session ID
+    MONGODB: process.env.MONGODB || 'mongodb://mongo:mongodb://mongo:dccsmLClukfKNxmhZVrmsdScwKiOtGbf@autorack.proxy.rlwy.net:44877:27017',// Enter Your MongoDB URL
+    OWNER_NUMBER: process.env.OWNER_NUMBER || '94741140620',    // Enter Your Owner Number
 };
